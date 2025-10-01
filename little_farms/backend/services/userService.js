639f0a3c24a7f7866c2ab3703578e9ca60709a09
rikamiyachi <rikamiyachi.2023@scis.smu.edu.sg>
2025-10-02 00:18:19 +0800

    Comment out getUserByEmail function in userService
    The getUserByEmail function and its db import have been commented out, because i no longer need to get users by email.

--- little_farms/backend/services/userService.js
+++ little_farms/backend/services/userService.js
@@ -1,15 +1,15 @@
-import { db } from "../adminFirebase.js";
+// import { db } from "../adminFirebase.js";
 
-export async function getUserByEmail(email) {
-  try {
-    const usersRef = db.collection('Users')
-    const snapshot = await usersRef.where('email', '==', email).get()
-    if (snapshot.empty) return null;
+// export async function getUserByEmail(email) {
+//   try {
+//     const usersRef = db.collection('Users')
+//     const snapshot = await usersRef.where('email', '==', email).get()
+//     if (snapshot.empty) return null;
 
-    const userDoc = snapshot.docs[0];
-    return { id: userDoc.id, ...userDoc.data() };
-  } catch (err) {
-    console.error("Error fetching user:", err);
-    return null;
-  }
-}
+//     const userDoc = snapshot.docs[0];
+//     return { id: userDoc.id, ...userDoc.data() };
+//   } catch (err) {
+//     console.error("Error fetching user:", err);
+//     return null;
+//   }
+// }
